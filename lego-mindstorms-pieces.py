a02f7cead99b091ca277c1228c12e2c058a26dd3
Peter Bittner <django@bittner.it>
2015-12-28 02:03:47 +0100

    Use BrickSet data for generation of combined list

--- lego-mindstorms-pieces.py
+++ lego-mindstorms-pieces.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
+#
+#    LEGO Mindstorms Editions Pieces Comparison
+#    Copyright (C) 2015  Peter Bittner <django@bittner.it>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 """
 A simple python program to find out which pieces you need to buy when
-you have bought Lego Mindstorms EV3 Home Edition (31313) and the Education
+you have bought LEGO Mindstorms EV3 Home Edition (31313) and the Education
 Expansion Set (45560).  So you can make all robots that can be made with
 the Education Core Set (45544) + Education Expansion Set.
 """
@@ -14,16 +31,19 @@ NO_PARTS = [0 for a in range(len(files))]
 for file_count, name in enumerate(files):
     print('Reading file: %s' % name, file=sys.stderr)
     with open(name) as f:
-        for line in f.readlines():
+        data_lines = f.readlines()[1:]
+        for line in data_lines:
             line = line.strip()
             try:
-                count, part_no = line.split('\t')
-                count, part_no = int(count), int(part_no)
+                set_no, part_no, quantity, color, category, design_id, \
+                part_name, image_url, set_count = line.split('\t')
+
+                part_no, quantity = int(part_no), int(quantity)
 
                 if part_no not in part_list.keys():
                     part_list[part_no] = NO_PARTS.copy()
 
-                part_list[part_no][file_count] = count
+                part_list[part_no][file_count] = quantity
             except ValueError as err:
                 print('Ignoring error: %s (%s)' % (err, line),
                       file=sys.stderr)
@@ -31,7 +51,7 @@ for file_count, name in enumerate(files):
 part_numbers = list(part_list.keys())
 part_numbers.sort()
 
-print('Part no.\t%s' % '\t'.join(files))
+print('Part no.\t%s\tPart name' % '\t'.join(files))
 for part_no in part_numbers:
     part_counts = '\t'.join([str(a) for a in part_list[part_no]])
     print('%s\t%s' % (part_no, part_counts))
